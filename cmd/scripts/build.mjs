1ccd41874cf8229ceeef8d0984889ee4f6e43dfb
rankangan <rankangan@wps.cn>
2023-09-04 14:50:00 +0800

    feat: update

--- cmd/scripts/build.mjs
+++ cmd/scripts/build.mjs
@@ -1,9 +1,46 @@
 import * as esbuild from 'esbuild'
 
+// const builds = ['index', 'kk'].map((entry) => {
+//   const entryPoint = `src/${entry}.ts`
+//   const outPoint = `bin/${entry}.cjs`
+//   return esbuild.build({
+//     bundle: true,
+//     entryPoints: [entryPoint],
+//     outfile: outPoint,
+//     format: 'cjs',
+//     platform: 'node',
+//     target: 'node14',
+//     plugins: [
+//       {
+//         name: 'alias',
+//         setup({ onResolve, resolve }) {
+//           onResolve(
+//             { filter: /^prompts$/, namespace: 'file' },
+//             async ({ importer, resolveDir }) => {
+//               // we can always use non-transpiled code since we support 14.16.0+
+//               const result = await resolve('prompts/lib/index.js', {
+//                 importer,
+//                 resolveDir,
+//                 kind: 'import-statement',
+//               })
+//               return result
+//             },
+//           )
+//         },
+//       },
+//     ],
+//   })
+// })
+
+// await Promise.all(builds)
+
 await esbuild.build({
   bundle: true,
-  entryPoints: ['src/kk.ts'],
-  outfile: 'bin/kk.cjs',
+  entryPoints: ['src/index.ts'],
+  outdir: 'bin',
+  outExtension: {
+    '.js': '.cjs',
+  },
   format: 'cjs',
   platform: 'node',
   target: 'node14',
--- cmd/src/index.ts
+++ cmd/src/index.ts
@@ -1,3 +1,62 @@
 #!/usr/bin/env node
 
-// npm create kk
+// kk create
+
+// import process from 'node:process'
+import fs from 'node:fs'
+import path from 'node:path'
+import chalk from 'chalk'
+import { Command } from 'commander'
+import createStarter from './createStarter.js'
+
+const pkg = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json')).toString())
+
+const program = new Command()
+
+program
+  .name('kk')
+  .description("😄 kk's simple cli to create commonly used templates.")
+  .version(pkg.version, '-v, --version')
+  // create-kk
+  .action(async () => {
+    await createStarter()
+  })
+
+program
+  .command('create')
+  .description(
+    `
+create template of different types, eg:
+👉 ${chalk.greenBright('kk create react')}
+👉 ${chalk.greenBright('kk create vue')}
+👉 ${chalk.greenBright('kk create lib')}
+...
+  `,
+  )
+  // -t 参数，目前只有starter模板，故注释掉
+  // .option('-t --type [type]', 'type to create', 'starter')
+  .action(async (args) => {
+    // create others
+    await createStarter()
+  })
+
+program.parse(process.argv)
+
+function sighandle() {
+  console.log(chalk.greenBright('👋 bye bye ~'))
+  process.exit(0)
+}
+
+process
+  .on('SIGINT', sighandle)
+  .on('SIGTERM', sighandle)
+  .on('SIGBREAK', sighandle)
+  .on('SIGUSR1', sighandle)
+
+// 捕获错误
+process.on('uncaughtException', (err, origin) => {
+  console.error(chalk.red(`📢 出错了 👇`))
+  console.error(`❌ ${chalk.redBright(origin)}: ${chalk.redBright(err.message)}`)
+  console.error(`🐛 ${chalk.red(err.stack)}`)
+  console.log()
+})
